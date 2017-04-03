661f4f480eb285e516b1f0bb9604300ae718ebf3
Scott Mikula <mikula@gmail.com>
2017-04-03 11:30:10 -0700

    Initial implementation of wrappers for then and catch callbacks

--- packages/satcheljs-promise/lib/actionWrappers.ts
+++ packages/satcheljs-promise/lib/actionWrappers.ts
@@ -1,3 +1,6 @@
+import { action } from 'satcheljs';
+import { getCurrentAction } from './promiseMiddleware';
+
 export type ThenType = typeof Promise.prototype.then;
 export type CatchType = typeof Promise.prototype.catch;
 
@@ -9,10 +12,33 @@ export function setOriginalThenCatch(thenValue: ThenType, catchValue: CatchType)
     originalThen = catchValue;
 }
 
-export function wrappedThen() {
-    originalThen.apply(this, arguments);
+export function wrappedThen(onFulfilled?: Function, onRejected?: Function) {
+    return originalThen.call(
+        this,
+        wrapInAction(onFulfilled, "then"),
+        wrapInAction(onRejected, "then_rejected"));
 }
 
-export function wrappedCatch() {
-    originalCatch.apply(this, arguments);
+export function wrappedCatch(onRejected?: Function) {
+    return originalCatch.call(
+        this,
+        wrapInAction(onRejected, "catch"));
+}
+
+function wrapInAction(callback: Function, callbackType: string) {
+    let currentAction = getCurrentAction();
+    if (!currentAction || !callback) {
+        return callback;
+    }
+
+    let actionName = currentAction + "->" + callbackType
+    return function () {
+        let returnValue;
+        let args = arguments;
+        action(actionName)(() => {
+            returnValue = callback.apply(null, args);
+        })();
+
+        return returnValue;
+    }
 }
