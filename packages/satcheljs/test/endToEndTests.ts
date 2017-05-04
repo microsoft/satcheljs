4df27f9147050650fda9182aa733c1bbf213c11f
Scott Mikula <mikula@gmail.com>
2017-05-04 16:01:10 -0700

    Add an end to end test

---
+++ packages/satcheljs/test/endToEndTests.ts
@@ -0,0 +1,33 @@
+import 'jasmine';
+import actionDispatcher from '../lib/actionDispatcher';
+import mutator from '../lib/mutator';
+
+
+describe("satcheljs", () => {
+
+    it("mutators subscribe to actions", () => {
+        let fooValue = null
+
+        // Create an action dispatcher
+        let testAction = actionDispatcher("testAction")(
+            function testAction(foo: string) {
+                return {
+                    type: "testAction",
+                    foo: foo
+                };
+            });
+
+        // Create a mutator that subscribes to it
+        mutator(testAction)(
+            function(actionMessage) {
+                fooValue = (actionMessage as any).foo;
+            });
+
+        // Dispatch the action
+        testAction("test");
+
+        // Validate that the mutator was called with the dispatched action
+        expect(fooValue).toBe("test");
+    });
+
+});
