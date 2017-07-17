79e101fe5a3aeef577a376da910975f50d64c6e6
Kenneth Chau <kenotron@users.noreply.github.com>
2017-07-17 15:40:50 -0700

    Fix select to be less confusing. (#66)
    * remove strange select() behavior with reaction
    
    * 2.15.0
    
    * update version

--- packages/satcheljs/lib/select.ts
+++ packages/satcheljs/lib/select.ts
@@ -1,63 +1,20 @@
-import {Reaction, Atom, IObservableValue, isObservableArray} from 'mobx';
-import {getOriginalTarget, getActionType, setActionType} from './functionInternals';
-import {getGlobalContext} from './globalContext';
+import { Reaction, Atom, IObservableValue, isObservableArray } from 'mobx';
+import { getOriginalTarget, getActionType, setActionType } from './functionInternals';
+import { getGlobalContext } from './globalContext';
 
-export type SelectorFunction<T> = {
-    [key in keyof T]?: (...args: any[]) => T[key];
-}
+export type SelectorFunction<T> = { [key in keyof T]?: (...args: any[]) => T[key] };
 
 function createCursorFromSelector<T>(selector: SelectorFunction<T>, args?: any) {
     let state: any = {};
-    let reaction = new Reaction('__SELECT__', null);
 
     Object.keys(selector).forEach((key: keyof T) => {
         if (typeof state[key] === typeof undefined) {
-            if (args && args.length > 0) {
-                reaction.track(() => selector[key].apply(null, args));
-            } else {
-                reaction.track(selector[key]);
-            }
-
-            let observable = reaction.observing[reaction.observing.length - 1];
-
-            if (observable && (<any>observable).get) {
-                let value: IObservableValue<any> = <any>observable;
-
-                Object.defineProperty(state, key, {
-                    enumerable: true,
-                    get: value.get.bind(observable),
-                    set: value.set.bind(observable)
-                });
-            } else if (observable && observable instanceof Atom) {
-                let parent: any[] = reaction.observing.length > 2 && <any>reaction.observing[reaction.observing.length - 2];
-                if (parent && isObservableArray(parent)) {
-                    // Handle the case where this is an element of an array
-                    let atom: Atom = observable;
-                    let index = parent.indexOf(atom);
-
-                    Object.defineProperty(state, key, {
-                        enumerable: true,
-                        get: () => selector[key].apply(null, args),
-                        set: (value) => parent[index] = value
-                    });
-                } else {
-                    // If not an array, then all we can provide is a getter
-                    Object.defineProperty(state, key, {
-                        enumerable: true,
-                        get: () => selector[key].apply(null, args)
-                    });
-                }
-            } else {
-                // If this is not an observable, then just create a getter
-                Object.defineProperty(state, key, {
-                    enumerable: true,
-                    get: () => selector[key].apply(null, args)
-                });
-            }
+            Object.defineProperty(state, key, {
+                enumerable: true,
+                get: () => selector[key].apply(null, args),
+            });
         }
     });
-
-    reaction.dispose();
 
     return state;
 }
@@ -90,11 +47,10 @@ export default function select<T>(selector: SelectorFunction<T>) {
                 }
                 args[argumentPosition] = state;
             }
-
             return target.apply(context, args);
-        }
+        };
 
         setActionType(returnValue, getActionType(<any>target));
         return <Target>returnValue;
-    }
+    };
 }
--- packages/satcheljs/test/selectTest.ts
+++ packages/satcheljs/test/selectTest.ts
@@ -1,211 +1,105 @@
 import 'jasmine';
-import {map, ObservableMap} from 'mobx';
 import action from '../lib/action';
 import select from '../lib/select';
 import createStore from '../lib/createStore';
-import {initializeTestMode, resetTestMode} from '../lib/testMode';
+import { initializeTestMode, resetTestMode } from '../lib/testMode';
 import { getActionType } from '../lib/functionInternals';
 
-describe("select", () => {
-    it("creates a state scoped to subset of state tree", () => {
+describe('select', () => {
+    it('creates a state scoped to subset of state tree', () => {
         let fooStore = createStore('foo', {
             key1: 'value1',
             obj1: {
                 obj1Key1: 'obj1_value1',
                 obj1Key2: 'obj1_value2',
             },
-            array1: ['array1_value1', 'array1_value2']
+            array1: ['array1_value1', 'array1_value2'],
         });
 
         interface ReadOnlyActionState {
-            key1: string,
-            obj1Key1: string,
-            obj1: any,
-            array1: string[],
-            array1Value1: string
-        };
+            key1: string;
+            obj1Key1: string;
+            obj1: any;
+            array1: string[];
+            array1Value1: string;
+        }
 
-        let readOnlyAction = action("readOnly")((state?: ReadOnlyActionState) => {
-            expect(state.key1).toBe(fooStore.key1);
-            expect(state.obj1Key1).toBe(fooStore.obj1.obj1Key1);
-            expect(state.obj1).toBe(fooStore.obj1);
-            expect(state.array1).toBe(fooStore.array1);
-            expect(state.array1Value1).toBe(fooStore.array1[0]);
-        });
+        let readOnlyAction = jasmine.createSpy(
+            'action',
+            action('readOnly')((state?: ReadOnlyActionState) => {
+                expect(state.key1).toBe(fooStore.key1);
+                expect(state.obj1Key1).toBe(fooStore.obj1.obj1Key1);
+                expect(state.obj1).toBe(fooStore.obj1);
+                expect(state.array1).toBe(fooStore.array1);
+                expect(state.array1Value1).toBe(fooStore.array1[0]);
+            })
+        );
 
         let newAction = select({
             key1: () => fooStore.key1,
             obj1Key1: () => fooStore.obj1.obj1Key1,
             obj1: () => fooStore.obj1,
             array1: () => fooStore.array1,
-            array1Value1: () => fooStore.array1[0]
+            array1Value1: () => fooStore.array1[0],
         })(readOnlyAction);
 
         newAction();
+
+        expect(readOnlyAction).toHaveBeenCalledTimes(1);
     });
 
-    it("can update observable value", () => {
-        let fooStore = createStore('foo', {
-            k: 'v',
-        });
-
-        interface ActionState {
-            key: string,
-        };
-
-        let updateAction = action("update")((state?: ActionState) => {
-            expect(state.key).toBe(fooStore.k);
-            state.key = 'newValue';
-        });
-
-        let newAction = select({
-            key: () => fooStore.k,
-        })(updateAction);
-
-        newAction();
-
-        expect(fooStore.k).toBe('newValue');
-    });
-
-    it("can update observable map", () => {
-        let fooStore = createStore('foo', {
-            map: map({k: 'v'})
-        });
-
-        interface ActionState {
-            map: ObservableMap<string>,
-        };
-
-        let updateAction = action("update")((state?: ActionState) => {
-            state.map.set('k', 'newValue');
-        });
-
-        let newAction = select({
-            map: () => fooStore.map,
-        })(updateAction);
-
-        newAction();
-
-        expect(fooStore.map.get('k')).toBe('newValue');
-    });
-
-    it("can update observable arrays", () => {
-        let fooStore = createStore('foo', {
-            array: ['v']
-        });
-
-        interface ActionState {
-            array: string[],
-        };
-
-        let updateAction = action("update")((state?: ActionState) => {
-            state.array.push('newValue');
-        });
-
-        let newAction = select({
-            array: () => fooStore.array,
-        })(updateAction);
-
-        newAction();
-
-        expect(fooStore.array[0]).toBe('v');
-        expect(fooStore.array[1]).toBe('newValue');
-        expect(fooStore.array.length).toBe(2);
-    });
-
-    it("can update atoms", () => {
-        let fooStore = createStore('foo', {
-            array: ['v']
-        });
-
-        interface ActionState {
-            array: string[],
-        };
-
-        let updateAction = action("update")((state?: ActionState) => {
-            state.array[0] = 'newValue';
-        });
-
-        let newAction = select({
-            array: () => fooStore.array,
-        })(updateAction);
-
-        newAction();
-
-        expect(fooStore.array[0]).toBe('newValue');
-        expect(fooStore.array.length).toBe(1);
-    });
-
-    it("can update properties of an observable object", () => {
-        let fooStore = createStore('foo', {
-            obj: {
-                k: 'v'
-            }
-        });
-
-        interface ActionState {
-            obj: {[key: string]: string},
-        };
-
-        let updateAction = action("update")((state?: ActionState) => {
-            state.obj['k'] = 'newValue';
-        });
-
-        let newAction = select({
-            obj: () => fooStore.obj,
-        })(updateAction);
-
-        newAction();
-
-        expect(fooStore.obj.k).toBe('newValue');
-    });
-
-    it("propagates action params to the selector function", () => {
+    it('propagates action params to the selector function', () => {
         let fooStore: any = createStore('foo', {
             id0: 'value',
-            array0: ['a', 'b', 'c']
+            array0: ['a', 'b', 'c'],
         });
 
-        let readAction = action("read")(function readAction(id: string, arrayIndex: number, state?: any) {
+        let readAction = action('read')(function readAction(
+            id: string,
+            arrayIndex: number,
+            state?: any
+        ) {
             expect(state.value).toBe('value');
             expect(state.arrayValue).toBe('c');
         });
 
         let newAction = select({
             value: (id: string, arrayIndex: number) => fooStore[id],
-            arrayValue: (id: string, arrayIndex: number) => fooStore.array0[arrayIndex]
+            arrayValue: (id: string, arrayIndex: number) => fooStore.array0[arrayIndex],
         })(readAction);
 
         newAction('id0', 2);
     });
 
-    it("places state at the right argument position even if the wrapped function has optional arguments before state", () => {
+    it('places state at the right argument position even if the wrapped function has optional arguments before state', () => {
         let fooStore: any = createStore('foo', {
-            key: 'value'
+            key: 'value',
         });
 
-        let someAction = action("someAction")((required: string, optional?: string, state?: any) => {
+        let someAction = action(
+            'someAction'
+        )((required: string, optional?: string, state?: any) => {
             expect(state.key).toBe('value');
             expect(optional).not.toBeDefined();
             expect(required).toBe('required value');
         });
 
         let newAction = select({
-            key: () => fooStore.key
+            key: () => fooStore.key,
         })(someAction);
 
         newAction('required value');
     });
 
-    it("can handle having action be the outer decorator", () => {
+    it('can handle having action be the outer decorator', () => {
         let fooStore: any = createStore('foo', {
-            key: 'value'
+            key: 'value',
         });
 
         let functionIsCalled = false;
 
         let someAction = select({
-            key: () => fooStore.key
+            key: () => fooStore.key,
         })((required: string, optional?: string, state?: any) => {
             expect(state.key).toBe('value');
             expect(optional).not.toBeDefined();
@@ -213,32 +107,32 @@ describe("select", () => {
             functionIsCalled = true;
         });
 
-        let newAction = action("action")(someAction);
+        let newAction = action('action')(someAction);
 
         newAction('required value');
 
         expect(functionIsCalled).toBeTruthy();
     });
 
-    it("allows tests to passthrough state param", () => {
+    it('allows tests to passthrough state param', () => {
         let fooStore: any = createStore('foo', {
-            key: 'value'
+            key: 'value',
         });
 
         let someAction = select({
-            key: () => fooStore.key
+            key: () => fooStore.key,
         })((required: string, optional?: string, state?: any) => {
             expect(state.key).toBe('testValue');
             expect(optional).toBe('optional');
             expect(required).toBe('required value');
         });
 
-        let newAction = action("action")(someAction);
+        let newAction = action('action')(someAction);
 
-        newAction('required value', 'optional', {key: 'testValue'});
+        newAction('required value', 'optional', { key: 'testValue' });
     });
 
-    it("can use new TS 2.1 mapped types to describe selector functions", () => {
+    it('can use new TS 2.1 mapped types to describe selector functions', () => {
         let fooStore = createStore('foo', {
             k: 'v',
         });
@@ -247,9 +141,8 @@ describe("select", () => {
             key: string;
         }
 
-        let updateAction = action("update")((state?: ActionState) => {
+        let updateAction = action('update')((state?: ActionState) => {
             expect(state.key).toBe(fooStore.k);
-            state.key = 'newValue';
         });
 
         let newAction = select<ActionState>({
@@ -257,18 +150,16 @@ describe("select", () => {
         })(updateAction);
 
         newAction();
-
-        expect(fooStore.k).toBe('newValue');
     });
 
-    it("does not execute the selector function in test mode", () => {
+    it('does not execute the selector function in test mode', () => {
         initializeTestMode();
 
-        let fooSelector = jasmine.createSpy(null);
+        let fooSelector = jasmine.createSpy('fooSelector');
         let actionSpy = jasmine.createSpy('action');
 
         select({
-            foo: fooSelector
+            foo: fooSelector,
         })(actionSpy)();
 
         expect(fooSelector).not.toHaveBeenCalled();
@@ -277,24 +168,25 @@ describe("select", () => {
         resetTestMode();
     });
 
-    it("does execute the selector function after test mode has been cleared", () => {
+    it('does execute the selector function after test mode has been cleared', () => {
         initializeTestMode();
         resetTestMode();
 
-        let fooSelector = jasmine.createSpy(null);
-        let actionSpy = jasmine.createSpy('action');
+        let fooSelector = jasmine.createSpy('fooSelector').and.returnValue('fooValue');
+        let action = (state?: any) => {
+            expect(state.foo).toBe('fooValue');
+        };
 
         select({
-            foo: fooSelector
-        })(actionSpy)();
+            foo: fooSelector,
+        })(action)();
 
         expect(fooSelector).toHaveBeenCalled();
-        expect(actionSpy).toHaveBeenCalled();
     });
 
-    it("when wrapping an action, preserves the action type", () => {
+    it('when wrapping an action, preserves the action type', () => {
         // Arrange
-        let actionName = "testAction";
+        let actionName = 'testAction';
         let testAction = action(actionName)(() => {});
 
         // Act
