b0dfbd90fa3fbaee14b56fb079ca5ee836edd8cc
jesomm <jesomm@microsoft.com>
2017-08-09 14:16:21 -0700

    fix select returning stale data bug (#69)
    * fix module cycle
    
    * Fix select to be less confusing. (#66)
    
    * remove strange select() behavior with reaction
    
    * 2.15.0
    
    * update version
    
    * Revert "fix module cycle"
    
    This reverts commit 381fd128d3a3e2c63d45e8fba5357c5ac3c4773b.
    
    * fix revert 381fd12
    
    * fix package version
    
    * rename so jest will run tests & then fix broken tests

--- test/legacy/selectTests.ts
+++ test/legacy/selectTests.ts
@@ -1,5 +1,4 @@
 import 'jasmine';
-import { map, ObservableMap } from 'mobx';
 import action from '../../src/legacy/action';
 import select from '../../src/legacy/select';
 import createStore from '../../src/createStore';
@@ -25,13 +24,16 @@ describe('select', () => {
             array1Value1: string;
         }
 
-        let readOnlyAction = action('readOnly')((state?: ReadOnlyActionState) => {
-            expect(state.key1).toBe(fooStore.key1);
-            expect(state.obj1Key1).toBe(fooStore.obj1.obj1Key1);
-            expect(state.obj1).toBe(fooStore.obj1);
-            expect(state.array1).toBe(fooStore.array1);
-            expect(state.array1Value1).toBe(fooStore.array1[0]);
-        });
+        let readOnlyAction = jasmine.createSpy(
+            'action',
+            action('readOnly')((state?: ReadOnlyActionState) => {
+                expect(state.key1).toBe(fooStore.key1);
+                expect(state.obj1Key1).toBe(fooStore.obj1.obj1Key1);
+                expect(state.obj1).toBe(fooStore.obj1);
+                expect(state.array1).toBe(fooStore.array1);
+                expect(state.array1Value1).toBe(fooStore.array1[0]);
+            })
+        );
 
         let newAction = select({
             key1: () => fooStore.key1,
@@ -42,124 +44,8 @@ describe('select', () => {
         })(readOnlyAction);
 
         newAction();
-    });
 
-    it('can update observable value', () => {
-        let fooStore = createStore('foo', {
-            k: 'v',
-        })();
-
-        interface ActionState {
-            key: string;
-        }
-
-        let updateAction = action('update')((state?: ActionState) => {
-            expect(state.key).toBe(fooStore.k);
-            state.key = 'newValue';
-        });
-
-        let newAction = select({
-            key: () => fooStore.k,
-        })(updateAction);
-
-        newAction();
-
-        expect(fooStore.k).toBe('newValue');
-    });
-
-    it('can update observable map', () => {
-        let fooStore = createStore('foo', {
-            map: map({ k: 'v' }),
-        })();
-
-        interface ActionState {
-            map: ObservableMap<string>;
-        }
-
-        let updateAction = action('update')((state?: ActionState) => {
-            state.map.set('k', 'newValue');
-        });
-
-        let newAction = select({
-            map: () => fooStore.map,
-        })(updateAction);
-
-        newAction();
-
-        expect(fooStore.map.get('k')).toBe('newValue');
-    });
-
-    it('can update observable arrays', () => {
-        let fooStore = createStore('foo', {
-            array: ['v'],
-        })();
-
-        interface ActionState {
-            array: string[];
-        }
-
-        let updateAction = action('update')((state?: ActionState) => {
-            state.array.push('newValue');
-        });
-
-        let newAction = select({
-            array: () => fooStore.array,
-        })(updateAction);
-
-        newAction();
-
-        expect(fooStore.array[0]).toBe('v');
-        expect(fooStore.array[1]).toBe('newValue');
-        expect(fooStore.array.length).toBe(2);
-    });
-
-    it('can update atoms', () => {
-        let fooStore = createStore('foo', {
-            array: ['v'],
-        })();
-
-        interface ActionState {
-            array: string[];
-        }
-
-        let updateAction = action('update')((state?: ActionState) => {
-            state.array[0] = 'newValue';
-        });
-
-        let newAction = select({
-            array: () => fooStore.array,
-        })(updateAction);
-
-        newAction();
-
-        expect(fooStore.array[0]).toBe('newValue');
-        expect(fooStore.array.length).toBe(1);
-    });
-
-    it('can update properties of an observable object', () => {
-        let fooStore = createStore('foo', {
-            obj: {
-                k: 'v',
-            },
-        })();
-
-        interface ActionState {
-            obj: { [key: string]: string };
-        }
-
-        let updateAction = action('update')((state?: ActionState) => {
-            /* tslint:disable:no-string-literal */
-            state.obj['k'] = 'newValue';
-            /* tslint:enable:no-string-literal */
-        });
-
-        let newAction = select({
-            obj: () => fooStore.obj,
-        })(updateAction);
-
-        newAction();
-
-        expect(fooStore.obj.k).toBe('newValue');
+        expect(readOnlyAction).toHaveBeenCalledTimes(1);
     });
 
     it('propagates action params to the selector function', () => {
@@ -257,7 +143,6 @@ describe('select', () => {
 
         let updateAction = action('update')((state?: ActionState) => {
             expect(state.key).toBe(fooStore.k);
-            state.key = 'newValue';
         });
 
         let newAction = select<ActionState>({
@@ -265,14 +150,12 @@ describe('select', () => {
         })(updateAction);
 
         newAction();
-
-        expect(fooStore.k).toBe('newValue');
     });
 
     it('does not execute the selector function in test mode', () => {
         initializeTestMode();
 
-        let fooSelector = jasmine.createSpy(null);
+        let fooSelector = jasmine.createSpy('fooSelector');
         let actionSpy = jasmine.createSpy('action');
 
         select({
@@ -289,15 +172,16 @@ describe('select', () => {
         initializeTestMode();
         resetTestMode();
 
-        let fooSelector = jasmine.createSpy(null);
-        let actionSpy = jasmine.createSpy('action');
+        let fooSelector = jasmine.createSpy('fooSelector').and.returnValue('fooValue');
+        let action = (state?: any) => {
+            expect(state.foo).toBe('fooValue');
+        };
 
         select({
             foo: fooSelector,
-        })(actionSpy)();
+        })(action)();
 
         expect(fooSelector).toHaveBeenCalled();
-        expect(actionSpy).toHaveBeenCalled();
     });
 
     it('when wrapping an action, preserves the action type', () => {
