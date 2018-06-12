b2c7e8687f173317b957403965f5349aa8b21565
Scott Mikula <mikula@gmail.com>
2018-06-12 14:58:56 -0700

    Add action handling to mutator

--- src/createMutator.ts
+++ src/createMutator.ts
@@ -1,11 +1,53 @@
-export class Mutator<T> {
-    constructor(private initialValue: T) {}
+import ActionCreator from './interfaces/ActionCreator';
+import ActionMessage from './interfaces/ActionMessage';
+import Subscriber from './interfaces/Subscriber';
+import mutator from './mutator';
+import { getPrivateActionId } from './actionCreator';
+
+export type MutatorHandler<TState, TAction extends ActionMessage> = (
+    state: TState,
+    action: TAction
+) => TState | void;
+
+export class Mutator<TState> {
+    private handlers: { [actionId: string]: MutatorHandler<TState, ActionMessage> } = {};
+
+    constructor(private initialValue: TState) {}
 
     getInitialValue() {
         return this.initialValue;
     }
+
+    handles<TAction extends ActionMessage>(
+        actionCreator: ActionCreator<TAction>,
+        handler: MutatorHandler<TState, TAction>
+    ) {
+        let actionId = getPrivateActionId(actionCreator);
+        if (this.handlers[actionId]) {
+            throw new Error('A mutator may not handle the same action twice.');
+        }
+
+        this.handlers[actionId] = handler;
+        return this;
+    }
+
+    handleAction(
+        currentState: TState,
+        actionMessage: ActionMessage,
+        replaceState: (newState: TState) => void
+    ) {
+        let actionId = getPrivateActionId(actionMessage);
+        let handler = this.handlers[actionId];
+        if (handler) {
+            let returnValue = handler(currentState, actionMessage);
+            if (returnValue !== undefined) {
+                // TODO: does checking for undefined work??
+                replaceState(<TState>returnValue);
+            }
+        }
+    }
 }
 
-export function createMutator<T>(initialValue: T): Mutator<T> {
+export function createMutator<TState>(initialValue: TState) {
     return new Mutator(initialValue);
 }
--- test/createMutatorTests.ts
+++ test/createMutatorTests.ts
@@ -1,6 +1,10 @@
+import { actionCreator } from '../src/index';
 import { createMutator } from '../src/createMutator';
 
 describe('createMutator', () => {
+    const testAction = actionCreator('testAction');
+    const actionToDispatch = testAction();
+
     it('returns a mutator with the given initial value', () => {
         // Arrange
         const initialValue = {};
@@ -10,5 +14,39 @@ describe('createMutator', () => {
 
         // Assert
         expect(mutator.getInitialValue()).toBe(initialValue);
+    });
+
+    it('can modify the state when handling an action', () => {
+        // Arrange
+        const state = { a: 1 };
+        const mutator = createMutator(state);
+        const replaceState = jasmine.createSpy('replaceState');
+
+        mutator.handles(testAction, (state, actionMessage) => {
+            state.a = 2;
+        });
+
+        // Act
+        mutator.handleAction(state, actionToDispatch, replaceState);
+
+        // Assert
+        expect(state).toEqual({ a: 2 });
+    });
+
+    it('can replace the state when handling an action', () => {
+        // Arrange
+        const state = { a: 1 };
+        const mutator = createMutator(state);
+        const replaceState = jasmine.createSpy('replaceState');
+
+        mutator.handles(testAction, (state, actionMessage) => {
+            return null;
+        });
+
+        // Act
+        mutator.handleAction(state, actionToDispatch, replaceState);
+
+        // Assert
+        expect(replaceState).toHaveBeenCalledWith(null);
     });
 });
