912d38eb8937f16364f44523d8e80830ebcfac88
Scott Mikula <mikula@gmail.com>
2018-06-26 14:14:08 -0700

    Fix MutatorMap typing

--- src/interfaces/MutatorMap.ts
+++ src/interfaces/MutatorMap.ts
@@ -1,5 +1,8 @@
 import Mutator from '../Mutator';
 import CombinedMutator from '../CombinedMutator';
 
-type MutatorMap<TState> = { [K in keyof TState]: Mutator<TState[K]> | CombinedMutator<TState[K]> };
+type MutatorMap<TState extends { [key: string]: any }> = {
+    [K in keyof TState]: Mutator<TState[K]> | CombinedMutator<TState[K]>
+};
+
 export default MutatorMap;
