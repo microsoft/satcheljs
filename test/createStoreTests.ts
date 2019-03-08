38cc26f0b45f3b43ccc8d2e0ca041f212959dcd8
Eric Allen <eric@ericrallen.com>
2019-03-08 00:06:11 -0500

    throw error if user tries to create a store that already exists

--- test/createStoreTests.ts
+++ test/createStoreTests.ts
@@ -16,4 +16,21 @@ describe('createStore', () => {
         expect(store).toEqual(initialState);
         expect(getRootStore().get('testStore')).toEqual(initialState);
     });
+
+    it('prevents creating a store with the same name', () => {
+        // Arrange
+        __resetGlobalContext();
+        let initialState = { testProp: 'testValue' };
+
+        let secondaryState = { testProp: 'overwritten' };
+
+        // Act
+        let store = createStore('testStore', initialState)();
+
+        // Assert
+        expect(() => createStore('testStore', secondaryState)()).toThrow(
+            'A store named testStore has already been created.'
+        );
+        expect(getRootStore().get('testStore')).toEqual(initialState);
+    });
 });
