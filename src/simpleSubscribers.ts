8eddeee363f68966a0c72aba0672a4d63f00f852
Scott Mikula <mikula@gmail.com>
2019-09-10 11:37:21 -0700

    Prohibit mutator actions from returning values or being async (#130)
    This addresses #129.  The fix was simple; we already have this check for normal mutators, so the fix is to pass the return value through so that the same check can apply to mutator actions.

--- src/simpleSubscribers.ts
+++ src/simpleSubscribers.ts
@@ -1,9 +1,6 @@
-import ActionCreator from './interfaces/ActionCreator';
 import SimpleAction from './interfaces/SimpleAction';
-import Subscriber from './interfaces/Subscriber';
 import { action } from './actionCreator';
 import mutator from './mutator';
-import orchestrator from './orchestrator';
 
 export function createSimpleSubscriber(decorator: Function) {
     return function simpleSubscriber<T extends SimpleAction>(actionType: string, target: T): T {
@@ -16,7 +13,7 @@ export function createSimpleSubscriber(decorator: Function) {
 
         // Create the subscriber
         decorator(simpleActionCreator, function simpleSubscriberCallback(actionMessage: any) {
-            target.apply(null, actionMessage.args);
+            return target.apply(null, actionMessage.args);
         });
 
         // Return a function that dispatches that action
